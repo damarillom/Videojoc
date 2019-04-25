06716baa594c3fdd32eab31400fbfa744615e20c
damarillom <danielamarillo1@hotmail.com>
2019-04-25 12:40:11 +0200

    Sin comentarios en Global.gd

--- Videojoc/Scripts/Global.gd
+++ Videojoc/Scripts/Global.gd
@@ -1,18 +1,5 @@
 extends Node
 
-'''var Player;
-var GameState;
-var GUI;
-
-var jump_sfx;
-var pain_sfx;
-
-var Level1="res://HoppyDays/Scenes/Levels/Level1.tscn"
-var GameOver="res://HoppyDays/Scenes/Levels/GameOver.tscn"
-var GameWin="res://HoppyDays/Scenes/Levels/GameWin.tscn"
-var Lightning="res://HoppyDays/Scenes/Lightning.tscn"'''
-
-
 func _ready():
 	pass # Replace with function body.
 
